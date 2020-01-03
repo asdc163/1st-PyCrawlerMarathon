823771e4c2e9ef6525aad3c0f590ccc446b6b7af
asdc163 <58109713+asdc163@users.noreply.github.com>
2020-01-03 11:49:48 +0800

    1/3

--- homework/main.py
+++ homework/main.py
@@ -4,13 +4,12 @@ from scrapy.utils.project import get_project_settings
 
 
 def main():
-    target_urls = [
-        'https://www.ptt.cc/bbs/Gossiping/M.1559788476.A.074.html',
-        'https://www.ptt.cc/bbs/Gossiping/M.1557928779.A.0C1.html'
-    ]
+    #target_board = ['Gossiping', 'Stock']
+    target_board = ['Gossiping']
     process = CrawlerProcess(get_project_settings())
-    process.crawl('PTTCrawler', start_urls=target_urls, filename='test.json')
-    process.start()
+    for board in target_board:
+        process.crawl('PTTCrawler', board=board)
+        process.start()
 
 if __name__ == '__main__':
     main()
